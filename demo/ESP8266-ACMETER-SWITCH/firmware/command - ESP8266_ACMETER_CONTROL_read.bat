c9cc5edd4b8739d7cedaa5d59a6fbc6918c4dfe4
yangbo1979 <gyangbo@gmail.com>
2016-10-24 10:29:59 +0800

    add firmware tools

---
+++ demo/ESP8266-ACMETER-SWITCH/firmware/command - ESP8266_ACMETER_CONTROL_read.bat
@@ -0,0 +1,1 @@
+esptool.py --baud 345600 --port COM12 read_flash 0 4194304 ESP8266_ACMETER_CONTROL.bin
---
+++ demo/ESP8266-ACMETER-SWITCH/firmware/command - ESP8266_ACMETER_CONTROL_write.bat
@@ -0,0 +1,1 @@
+esptool.py --baud 345600 --port COM12 write_flash -fm dio -fs 32m 0x00000 ESP8266_ACMETER_CONTROL.bin
