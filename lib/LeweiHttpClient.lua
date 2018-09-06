0c4ccd1415e02444fded3f30c03864f9734f0139
yangbo1979 <gyangbo@gmail.com>
2018-09-06 13:21:19 +0800

    update，using HTTP lib

--- lib/LeweiHttpClient.lua
+++ lib/LeweiHttpClient.lua
@@ -21,6 +21,7 @@ local moduleName = ...
 local M = {}
 _G[moduleName] = M
 local serverName = "open.lewei50.com"
+--local serverName = "192.168.0.5:81"
 local serverIP
 
 local gateWay
@@ -28,28 +29,93 @@ local userKey
 local sn
 local sensorValueTable
 local apiUrl = ""
+local apiLogUrl = ""
 local socket = nil
 
 function M.init(gw,ukey)
-     if(_G["gateWay"] ~= nil) then gateWay = _G["gateWay"]
-     else gateWay = gw
-     end
-     if(_G["userKey"] ~= nil) then userKey = _G["userKey"]
-     else userKey = ukey
-     end
-     	apiUrl = "UpdateSensors/"..gateWay
      if(_G["sn"] ~= nil) then sn = _G["sn"]
-     	apiUrl = "UpdateSensorsBySN/"..sn
+      apiUrl = "UpdateSensorsBySN/"..sn
+      apiLogUrl = "updatelogBySN/"..sn
+     else
+          if(_G["gateWay"] ~= nil) then gateWay = _G["gateWay"]
+          else gateWay = gw
+          end
+          if(_G["userKey"] ~= nil) then userKey = _G["userKey"]
+          else userKey = userkey
+          end
+     	apiUrl = "UpdateSensors/"..gateWay
+     	apiLogUrl = "updatelog/"..gateWay
      end
      sensorValueTable = {}
 end
 
 function M.appendSensorValue(sname,svalue)
-     sensorValueTable[""..sname]=""..svalue
+     --sensorValueTable[""..sname]=""..svalue
+     tmpTbl = {}
+     tmpTbl["name"]=sname
+     tmpTbl["value"]=svalue
+     table.insert(sensorValueTable,tmpTbl)
 end
 
 function M.sendSensorValue(sname,svalue)
-     if(wifi.sta.getip()==nil)then node.restart() end
+     M.appendSensorValue(sname,svalue)
+     --创建一个TCP连接
+     --socket=net.createConnection(net.TCP, 0)
+
+     --[[
+     --域名解析IP地址并赋值
+     if(serverIP == nil) then
+     socket:dns(serverName, function(conn, ip)
+          print("Connection IP:" .. ip)
+          serverIP = ip
+          end)     
+     end
+     ]]--
+     print(sjson.encode(sensorValueTable))
+
+     --if(serverIP ~= nil) then
+     http.post('http://'..serverName.."/api/V1/gateway/"..apiUrl,
+          userKey,
+          sjson.encode(sensorValueTable),
+          function(code, data)
+          if (code < 0) then
+           print("HTTP request failed")
+          else
+           print(code, data)
+          end
+     end)
+     sensorValueTable = {}
+     --[[
+     socket:connect(80, serverIP)
+     socket:on("connection", function(sck, response)
+          
+          --定义数据变量格式
+          PostData = "["
+          for i,v in pairs(sensorValueTable) do 
+               PostData = PostData .. "{\"Name\":\""..i.."\",\"Value\":\"" .. v .. "\"},"
+          end
+          PostData = PostData .."{\"Name\":\""..sname.."\",\"Value\":\"" .. svalue .. "\"}"
+          PostData = PostData .. "]"
+          --HTTP请求头定义
+          socket:send("POST /api/V1/gateway/"..apiUrl.." HTTP/1.1\r\n")
+          socket:send("Host: "..serverName.."\r\n")
+          socket:send("Content-Length: " .. string.len(PostData) .. "\r\n")
+          if(userKey~=nil) then socket:send("userkey: "..userKey.."\r\n") end
+          socket:send("\r\n"..PostData .. "\r\n")
+          end)
+     
+     --HTTP响应内容
+     socket:on("receive", function(sck, response)
+          --print(response)
+          PostData = nil
+          socket:close()
+          print(node.heap())
+        end)
+     ]]--
+     --end
+end
+
+function M.sendLog(logStr)
      --创建一个TCP连接
      socket=net.createConnection(net.TCP, 0)
 
@@ -65,41 +131,24 @@ function M.sendSensorValue(sname,svalue)
      
      socket:connect(80, serverIP)
      socket:on("connection", function(sck, response)
-          cntLen = 0
-          for i,v in pairs(sensorValueTable) do
-          cntLen = cntLen + string.len(i)+string.len(v)+23
-          end
-          cntLen = cntLen + string.len(sname)+string.len(svalue)+24
-
+          
           --定义数据变量格式
+          PostData = "{\"Message\":\"" .. logStr .. "\"}"
           --HTTP请求头定义
-          pl = ""
-          pl = pl .. "POST /api/V1/gateway/"..apiUrl.." HTTP/1.1\r\n"
-          pl = pl .. "Host: "..serverName.."\r\n"
-          pl = pl .. "Content-Length: " .. cntLen .. "\r\n"
-          if(userKey~=nil) then pl = pl .. "userkey: "..userKey.."\r\n" end
-          pl = pl .. "\r\n"
-          pl = pl .. "["
-          for i,v in pairs(sensorValueTable) do 
-               pl = pl .. "{\"Name\":\""..i.."\",\"Value\":\"" .. v .. "\"},"
-               --print(i)
-               --print(v) 
-          end
-          pl = pl .. "{\"Name\":\""..sname.."\",\"Value\":\"" .. svalue .. "\"}"
-          pl = pl .. "]"
-          socket:send(pl .. "\r\n")
+          print(apiLogUrl)
+          socket:send("POST /api/V1/gateway/"..apiLogUrl.." HTTP/1.1\r\n")
+          socket:send("Host: "..serverName.."\r\n")
+          socket:send("Content-Length: " .. string.len(PostData) .. "\r\n")
+          if(userKey~=nil) then socket:send("userkey: "..userKey.."\r\n") end
+          socket:send("\r\n"..PostData .. "\r\n")
           end)
-          --socket:on("sent", function(sck, response)
-               --print(tmr.now().."sent")
-          --sensorValueTable  = {}
-          --end)
      
      --HTTP响应内容
      socket:on("receive", function(sck, response)
-          --print(response)
+          print(response)
           PostData = nil
           socket:close()
-          --print(node.heap())
+          print("log send")
         end)
      end
 end
